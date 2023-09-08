cc4a354a2922b57f593b43baf422f9ea6eeb8e14
Malathi Soundarapandian <malathi.s@kermittech.no>
2023-09-08 12:42:50 +0000

    Chnages done in schema.yml for tests

--- models/customers.sql
+++ models/customers.sql
@@ -1,12 +1,12 @@
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+    select * from {{ ref('stg_customer') }}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('stg_order') }}
 
 ),
 
